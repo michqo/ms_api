45011ba9d644b6a8f57cd7baf567a7644e69f694
michqo <42279279+michqo@users.noreply.github.com>
2024-10-15 20:55:14 +0200

    refactor: drf generics

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import MeasurementDetail, MeasurementList
 
 urlpatterns = [
-    path('measurements/', MeasurementList.as_view(), name='measurement-list'),
-    path('measurements/<int:pk>/', MeasurementDetail.as_view(), name='measurement-detail'),
+    path('measurements', MeasurementList.as_view(), name='measurement-list'),
+    path('measurements/<int:pk>', MeasurementDetail.as_view(), name='measurement-detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -2,53 +2,21 @@ from django.http import Http404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 from .models import Measurement
 from .serializers import MeasurementSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .permissions import IsOwner
 from rest_framework.permissions import IsAuthenticated
 
-class MeasurementList(APIView):
+class MeasurementList(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated, IsOwner]
+    permission_classes = [IsAuthenticated]
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
 
-    def get(self, request):
-        measurements = Measurement.objects.all()
-        serializer = MeasurementSerializer(measurements, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = MeasurementSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class MeasurementDetail(APIView):
+class MeasurementDetail(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated, IsOwner]
-
-    def get_object(self, pk):
-        try:
-            return Measurement.objects.get(pk=pk)
-        except Measurement.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        measurement = self.get_object(pk)
-        serializer = MeasurementSerializer(measurement)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        measurement = self.get_object(pk)
-        serializer = MeasurementSerializer(measurement, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        measurement = self.get_object(pk)
-        measurement.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    permission_classes = [IsAuthenticated]
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
