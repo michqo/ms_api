e907894e510aafb188cf7f923e4f12d8a500580c
michqo <42279279+michqo@users.noreply.github.com>
2024-12-04 13:05:44 +0100

    feat: test script

---
+++ scripts/test.py
@@ -0,0 +1,58 @@
+import json
+import requests
+from datetime import datetime
+import random
+import sys
+import dataclasses
+from dataclasses import dataclass
+
+URL = "http://localhost:8000"
+ROUTE = "{}/api/measurements/".format(URL)
+TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzMzMzk2MjQ5LCJpYXQiOjE3MzMzMDk4NDksImp0aSI6ImZmZTNiMWI1NjkwYzQ3ZjRhYThlYzhjOTU5M2E1YTgwIiwidXNlcl9pZCI6MX0.3-gWhwlRF9rColt6QxWzAPwYVUP3evbIqSiuXl8PlDs"
+
+HEADERS = {
+    'Content-type': 'application/json',
+    'Authorization': f"JWT {TOKEN}"
+}
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime):
+            return o.isoformat() + 'Z'
+        return super().default(o)
+
+@dataclass
+class Temp:
+    timestamp: datetime
+    temperature: float = 0.0
+    humidity: float = 0.0
+    pressure: float = 0.0
+    rain: float = 0.0
+    wind_speed: float = 0.0
+    wind_direction: float = 0.0
+
+now = datetime.now()
+data = Temp(now)
+temps: list[dict] = []
+
+def main():
+    if len(sys.argv) > 1:
+        if sys.argv[1].isdigit():
+            data.timestamp = data.timestamp.replace(day=int(sys.argv[1]))
+    
+    for i in range (0, 24):
+        lower = random.uniform(21, 22.5)
+        upper = random.uniform(22.5, 24.5)
+        temp = random.uniform(lower, upper)
+        humidity = random.uniform(50, 70)
+        data.temperature = round(temp, 1)
+        data.humidity = round(humidity, 1)
+        data.timestamp = data.timestamp.replace(hour=i)
+        temps.append(dataclasses.asdict(data))
+
+    for temp in temps:
+        r = requests.post(ROUTE, headers=HEADERS, data=json.dumps(temp, cls=DateTimeEncoder))
+        print(r.text)
+
+if __name__ == "__main__":
+    main()
